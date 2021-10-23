607a866d64744a6c633d492899572dcabceb0321
ramaherdianto <ramadhanherdianto35@gmail.com>
2021-10-23 23:29:35 +0700

    menambahkan file

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/projectweb/';
+$config['base_url'] = 'http://localhost:8080/projectweb/';
 
 /*
 |--------------------------------------------------------------------------
